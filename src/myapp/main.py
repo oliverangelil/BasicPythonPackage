2bbf967fcc3d73787e8aa64bcfb1e6ee0fc8abb6
Oliver Angelil <olivermangelil@gmail.com>
2022-06-08 17:47:04 +0200

    first commit

---
+++ src/myapp/main.py
@@ -0,0 +1,3 @@
+import myapp.utils.utils_file as U
+
+ob = U.Dog('rover', 5)
---
+++ src/myapp/utils/utils_file.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
